25e6e677c9b1442239e4b7565efbc2bf7c33081a
TiagoLourenco1982 <eng.agro.tiagolourenco@gmail.com>
2024-05-15 02:31:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('menu-btn').addEventListener('click', function() {
+    var menu = document.getElementById('menu');
+    var menuWidth = menu.offsetWidth;
+    var menuBtn = document.getElementById('menu-btn');
+    if (menu.style.left === '0px') {
+        menu.style.left = `-${menuWidth}px`;
+        menuBtn.style.left = '20px'; // Movendo o botão para a posição inicial
+    } else {
+        menu.style.left = '0';
+        menuBtn.style.left = `${menuWidth + 20}px`; // Movendo o botão para a posição do menu
+    }
+});
