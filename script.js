91003f2822147cf44f573ccbf4c56b539784c3ea
TiagoLourenco1982 <eng.agro.tiagolourenco@gmail.com>
2024-05-15 19:30:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ document.getElementById('menu-btn').addEventListener('click', function() {
 
     if (menu.style.left === '0px') {
         menu.style.left = `-${menuWidth}px`;
+        menuBtn.style.left = '15px'; // Movendo o botão para a posição inicial
         menuBtn.style.left = '20px'; // Movendo o botão para a posição inicial
         mainContent.style.marginLeft = '20px'; // Ajuste da margem do conteúdo principal
     } else {
-        menu.style.left = '20';
+        menu.style.left = '0';
+        menuBtn.style.left = `${menuWidth -20}px`; // Movendo o botão para a posição do menu
         menuBtn.style.left = `${menuWidth - 10}px`; // Movendo o botão para a posição do menu
         mainContent.style.marginLeft = `${menuWidth}px`; // Ajuste da margem do conteúdo principal
     }
