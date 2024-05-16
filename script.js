4582092002fefcf8a6bf35fe06d17fc793c9a79f
TiagoLourenco1982 <eng.agro.tiagolourenco@gmail.com>
2024-05-16 18:19:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,15 +7,15 @@ document.getElementById('menu-btn').addEventListener('click', function() {
 
     if (menu.style.left === '0px' || menu.style.left === '') {
         menu.style.left = `-${menuWidth}px`;
-        menuBtn.style.left = '20px'; // Movendo o botão para a posição inicial
-        mainContent.style.marginLeft = '0'; // Resetando a margem esquerda do main
-        sidebar.style.marginLeft = '2%'; // Recuando o sidebar para a posição original
+        menuBtn.style.left = '13px'; // Movendo o botão para a posição inicial
+        mainContent.style.marginLeft = '200px'; // Resetando a margem esquerda do main
+        sidebar.style.marginLeft = '0'; // Resetando a margem esquerda do sidebar
         mainContent.classList.remove('main-expanded'); // Removendo a classe main-expanded quando o menu é recolhido
     } else {
         menu.style.left = '0';
-        menuBtn.style.left = `${menuWidth - 20}px`; // Movendo o botão para a posição do menu
-        mainContent.style.marginLeft = `${menuWidth}px`; // Ajustando a margem esquerda do main
-        sidebar.style.marginLeft = 'calc(2% + ' + menuWidth + 'px)'; // Ajustando a margem esquerda do sidebar
+        menuBtn.style.left = `${menuWidth + 13}px`; // Movendo o botão para a posição do menu
+        mainContent.style.marginLeft = `${menuWidth + 200}px`; // Ajustando a margem esquerda do main
+        sidebar.style.marginLeft = 'calc(0% + ' + menuWidth + 'px)'; // Ajustando a margem esquerda do sidebar
         mainContent.classList.add('main-expanded'); // Adicionando a classe main-expanded quando o menu é expandido
     }
 });
