0af4fe02d81ac7bb3a4dc1052cb5b31fcf7b974e
TiagoLourenco1982 <eng.agro.tiagolourenco@gmail.com>
2024-05-16 18:48:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,13 +7,15 @@ document.getElementById('menu-btn').addEventListener('click', function() {
 
     if (menu.style.left === '0px' || menu.style.left === '') {
         menu.style.left = `-${menuWidth}px`;
-        menuBtn.style.left = '13px'; // Movendo o botão para a posição inicial
+        menuBtn.style.left = '20px'; // Movendo o botão para a posição inicial
         mainContent.style.marginLeft = '0'; // Resetando a margem esquerda do main
-        sidebar.style.marginLeft = '0'; // Recuando o sidebar para a posição original
+        sidebar.style.marginLeft = '2%'; // Recuando o sidebar para a posição original
+        mainContent.classList.remove('main-expanded'); // Removendo a classe main-expanded quando o menu é recolhido
     } else {
         menu.style.left = '0';
-        menuBtn.style.left = `${menuWidth + 13}px`; // Movendo o botão para a posição do menu
+        menuBtn.style.left = `${menuWidth - 20}px`; // Movendo o botão para a posição do menu
         mainContent.style.marginLeft = `${menuWidth}px`; // Ajustando a margem esquerda do main
-        sidebar.style.marginLeft = `${menuWidth + 2}%`; // Ajustando a margem esquerda do sidebar
+        sidebar.style.marginLeft = 'calc(2% + ' + menuWidth + 'px)'; // Ajustando a margem esquerda do sidebar
+        mainContent.classList.add('main-expanded'); // Adicionando a classe main-expanded quando o menu é expandido
     }
 });
