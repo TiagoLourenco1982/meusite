d3bb8934cc53c819bca97c60927bea9f134e2b89
TiagoLourenco1982 <eng.agro.tiagolourenco@gmail.com>
2024-05-16 18:55:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-document.getElementById('menu-btn').addEventListener('click', function() {
+document.addEventListener('DOMContentLoaded', function() {
     var menu = document.getElementById('menu');
-    var menuWidth = menu.offsetWidth;
     var menuBtn = document.getElementById('menu-btn');
     var mainContent = document.querySelector('main');
     var sidebar = document.querySelector('.sidebar');
 
-    if (menu.style.left === '0px' || menu.style.left === '') {
-        menu.style.left = `-${menuWidth}px`;
-        menuBtn.style.left = '20px'; // Movendo o botão para a posição inicial
-        mainContent.style.marginLeft = '0'; // Resetando a margem esquerda do main
-        sidebar.style.marginLeft = '2%'; // Recuando o sidebar para a posição original
-        mainContent.classList.remove('main-expanded'); // Removendo a classe main-expanded quando o menu é recolhido
-    } else {
-        menu.style.left = '0';
-        menuBtn.style.left = `${menuWidth - 20}px`; // Movendo o botão para a posição do menu
-        mainContent.style.marginLeft = `${menuWidth}px`; // Ajustando a margem esquerda do main
-        sidebar.style.marginLeft = 'calc(2% + ' + menuWidth + 'px)'; // Ajustando a margem esquerda do sidebar
-        mainContent.classList.add('main-expanded'); // Adicionando a classe main-expanded quando o menu é expandido
-    }
+    // Define a posição inicial do menu
+    menu.style.left = '-80%'; // 100% - 20% (visível)
+
+    menuBtn.addEventListener('click', function() {
+        var menuWidth = menu.offsetWidth;
+
+        if (menu.style.left === '0px') {
+            menu.style.left = `-${menuWidth - 20}px`; // Menu parcialmente escondido com 20px visíveis
+            menuBtn.style.left = '20px'; // Posição inicial do botão
+            mainContent.style.marginLeft = '20px'; // Ajusta a margem esquerda do main
+            sidebar.style.marginLeft = 'calc(20px + 2%)'; // Ajusta a margem esquerda do sidebar
+            mainContent.classList.remove('main-expanded'); // Remove a classe main-expanded quando o menu é recolhido
+        } else {
+            menu.style.left = '0';
+            menuBtn.style.left = `${menuWidth}px`; // Movendo o botão para a posição do menu
+            mainContent.style.marginLeft = `${menuWidth}px`; // Ajustando a margem esquerda do main
+            sidebar.style.marginLeft = 'calc(2% + ' + menuWidth + 'px)'; // Ajustando a margem esquerda do sidebar
+            mainContent.classList.add('main-expanded'); // Adicionando a classe main-expanded quando o menu é expandido
+        }
+    });
 });
