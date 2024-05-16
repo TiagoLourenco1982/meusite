0044359c96a6f490c9413834822b58f858dfa6a2
TiagoLourenco1982 <eng.agro.tiagolourenco@gmail.com>
2024-05-15 22:39:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,13 @@ document.getElementById('menu-btn').addEventListener('click', function() {
     var menuBtn = document.getElementById('menu-btn');
     var mainContent = document.querySelector('main');
 
-    if (menu.style.left === '0px') {
+    if (menu.style.left === '0px' || menu.style.left === '') {
         menu.style.left = `-${menuWidth}px`;
-           menuBtn.style.left = '20px'; // Movendo o botão para a posição inicial
+        menuBtn.style.left = '20px'; // Movendo o botão para a posição inicial
         mainContent.classList.remove('main-expanded'); // Removendo a classe main-expanded quando o menu é recolhido
     } else {
-        menu.style.left = 'menuBtn.style.left = `${menuWidth - 20}px`; // Movendo o botão para a posição do menu
-
-        menuBtn.style.left = `${menuWidth -20}px`; // Movendo o botão para a posição do menu
-        menuBtn.style.left = `${menuWidth - 10}px`; // Movendo o botão para a posição do menu
+        menu.style.left = '15px'; // Definindo a posição do menu para 15 pixels da margem esquerda
+        menuBtn.style.left = `${menuWidth - 20}px`; // Movendo o botão para a posição do menu
         mainContent.classList.add('main-expanded'); // Adicionando a classe main-expanded quando o menu é expandido
     }
 });
